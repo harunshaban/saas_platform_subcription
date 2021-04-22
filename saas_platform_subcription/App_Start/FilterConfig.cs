55816b856ff90dee2fac0e44ddcd975742651a59
harunshaban <harun.sabban@gmail.com>
2021-04-23 01:21:41 +0200

    Add project files.

---
+++ saas_platform_subcription/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace saas_platform_subcription
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ saas_platform_subcription/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="saas_platform_subcription.MvcApplication" Language="C#" %>
